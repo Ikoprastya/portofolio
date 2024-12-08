f624e141e9e599838d57b12cf335b81a474383bc
Iko Prastya <ikoprastyaaaa@gmail.com>
2024-12-09 02:14:15 +0800

    Version 1

--- src/pages/AllProjectsPage.jsx
+++ src/pages/AllProjectsPage.jsx
@@ -13,47 +13,45 @@ const AllProjectsPage = () => {
       }, []);
 
     return (
-        <div className="container mx-auto py-10 px-48 ">
-            
+        <div className="container mx-auto pb-10 px-4 sm:px-10 md:px-20 lg:px-48">
             <nav className="text-sm text-gray-600 mb-4">
                 <span>
-                    <Link
-                    to="/"
-                    className="hover:underline"
-                    >
+                <Link to="/" className="hover:underline">
                     Home
-                    </Link>
+                </Link>
                 </span>
                 <span className="mx-2">/</span>
                 <span className="text-red-600">All Project</span>
             </nav>
-        
-            <h1 className="text-3xl font-bold pb-10">All Project</h1>
+
+            <h1 className="text-2xl sm:text-3xl font-bold pb-6 md:pb-10">All Project</h1>
             <div className="space-y-8">
                 {projectData.slice().reverse().map((project) => (
                 <div
                     key={project.id}
-                    className="flex flex-col md:flex-row items-center md:items-start gap-10 cursor-pointer border-b  border-red-600 pb-10"
+                    className="flex flex-col md:flex-row items-center md:items-start gap-6 md:gap-10 cursor-pointer border-b border-red-600 pb-6 md:pb-10"
                     onClick={() => navigate(`/projects/${project.id}`)}
                 >
                     <img
                     src={project.image}
                     alt={project.title}
-                    className="w-[500px] h-full object-cover rounded-md shadow-md"
+                    className="w-full sm:w-[400px] md:w-[500px] h-auto object-cover rounded-md shadow-md"
                     />
                     <div className="flex-1">
-                    <h3 className="text-2xl font-bold">{project.title}</h3>
-                    <div className="text-md text-[#8695A4] font-semibold my-4">
-                        <span className="text-white bg-red-600 rounded-xl font-bold px-2 mr-4">{`${project.year}`}</span> {`${project.category}`}
+                    <h3 className="text-lg sm:text-xl md:text-2xl font-bold">{project.title}</h3>
+                    <div className="text-sm sm:text-md text-[#8695A4] font-semibold my-2 sm:my-4">
+                        <span className="text-white bg-red-600 rounded-xl font-bold px-2 mr-4">
+                        {`${project.year}`}
+                        </span>{" "}
+                        {`${project.category}`}
                     </div>
-                    <p className="mt-2 text-gray-700">{project.description}</p>
+                    <p className="mt-2 text-gray-700 text-sm md:text-md lg:text-lg text-justify md:text-start">{project.description}</p>
                     </div>
                 </div>
-                
                 ))}
-            
             </div>
         </div>
+
     );
 };
 
--- src/pages/DetailProject.jsx
+++ src/pages/DetailProject.jsx
@@ -40,87 +40,73 @@ const ProjectDetailPage = () => {
   }
 
   return (
-    <div className="container mx-auto py-10 px-4 md:px-48">
-     
+    <div className="container mx-auto pb-10 px-4 sm:px-6 lg:px-48">
       <nav className="text-sm text-gray-600 mb-4">
         <span>
-            <Link
-              to="/"
-              className="hover:underline"
-            >
-              Home
-            </Link>
-          </span>
-          <span className="mx-2">/</span>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+        </span>
+        <span className="mx-2">/</span>
         <span>
-          <Link
-            to="/allproject"
-            className="hover:underline"
-          >
+          <Link to="/allproject" className="hover:underline">
             All Project
           </Link>
         </span>
         <span className="mx-2">/</span>
         <span className="text-red-600">{project.title}</span>
       </nav>
-      
-      <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
-      <div className="text-md text-[#8695A4] font-semibold my-6">
+
+      <h1 className="text-2xl sm:text-3xl font-bold mb-4">{project.title}</h1>
+      <div className="text-sm sm:text-md text-[#8695A4] font-semibold my-4 sm:my-6">
         <span className="text-white bg-red-600 rounded-xl font-bold px-2 mr-4">
           {project.year}
         </span>
         {project.category}
       </div>
-      <p className="mt-2 text-gray-700 w-3/4 mb-10">{project.description}</p>
+      <p className="text-sm md:text-md lg:text-lg text-gray-700 w-full md:w-3/4 mb-6 sm:mb-10">
+        {project.description}
+      </p>
 
       {/* Grid gambar */}
-      <div className="mt-6 grid grid-cols-1 gap-6">
+      <div className="mt-6 grid grid-cols-1  gap-6">
         {project.images.map((src, index) => (
           <div
             key={index}
-            className="relative group cursor-pointer shadow-md mb-6"
+            className="relative group cursor-pointer shadow-md"
             onClick={() => handleImageClick(index)}
           >
-            {/* Gambar */}
             <img
               src={src}
               alt={`${project.title} - Image ${index + 1}`}
-              className="w-full h-[630px] rounded-md object-cover object-top transition duration-300 ease-in-out group-hover:opacity-90"
+              className="w-full h-[300px] md:h-[630px] rounded-md object-cover object-top transition duration-300 ease-in-out group-hover:opacity-90"
             />
-            {/* Overlay gelap saat hover */}
             <div className="absolute inset-0 bg-black bg-opacity-25 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out"></div>
           </div>
         ))}
       </div>
 
-
-
-
       {/* Modal gambar */}
       {currentIndex !== null && (
         <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
-          {/* Gambar yang sedang ditampilkan */}
           <div className="relative">
             <img
               src={project.images[currentIndex]}
               alt={`Image ${currentIndex + 1}`}
               className="w-[90vw] h-[90vh] object-contain"
             />
-            {/* Tombol tutup */}
             <button
               onClick={handleCloseModal}
               className="absolute top-4 right-4 text-white text-2xl font-bold bg-red-600 rounded-full px-3 py-1"
             >
               ✕
             </button>
-            {/* Tombol navigasi kiri */}
             <button
               onClick={handlePrev}
               className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white text-3xl bg-gray-600 bg-opacity-50 hover:bg-gray-800 rounded-full px-3 py-1"
             >
               ❮
             </button>
-            {/* Tombol navigasi kanan */}
             <button
               onClick={handleNext}
               className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white text-3xl bg-gray-600 bg-opacity-50 hover:bg-gray-800 rounded-full px-3 py-1"
@@ -133,31 +119,29 @@ const ProjectDetailPage = () => {
 
       {/* Video Demonstrasi */}
       <div className="mt-10">
-        <h3 className="text-2xl font-semibold my-6">Video Demonstrasi</h3>
+        <h3 className="text-lg sm:text-2xl font-semibold my-4 sm:my-6">Video Demonstrasi</h3>
         <div className="bg-gray-900 p-4 rounded-md">
           <ReactPlayer
-            url={project.video} 
-            playing={false} 
-            controls={true} 
-            loop={true} 
+            url={project.video}
+            playing={false}
+            controls={true}
+            loop={true}
             width="100%"
-            height="40vh"
+            height="30vh"
             volume={0}
-            muted={true} 
+            muted={true}
             config={{
               file: {
                 attributes: {
-                  controlsList: "download", 
+                  controlsList: "download",
                 },
               },
             }}
           />
         </div>
       </div>
-
-
-
     </div>
+
   );
 };
 
